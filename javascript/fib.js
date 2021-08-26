05fe3e62d2835a74a044cb75aa4a0ed3c9efa13d
dmg59912 <dmg138380@yahoo.com>
2021-08-25 20:30:15 -0700

    done with fib

--- javascript/fib.js
+++ javascript/fib.js
@@ -5,6 +5,7 @@
 
 
 // first approach is to try recursion 
+// 2^n time complexity 0(n) space complexity or hight of the tree
 const fib = (n) => {
     if ( n <= 2 ) return 1;
 
@@ -13,6 +14,30 @@ const fib = (n) => {
 
 
 // test 
+console.log('Testing with good old recursive call ')
 console.log(fib(4));
 console.log(fib(7));
+console.log(fib(46));
 
+/*************************************************************************************************************************************
+ * ***********************************************************************************************************************************
+ * ***********************************************************************************************************************************
+ */
+
+// Different approach using dynamic memory using memoization to store duplicate values 
+// need to use javascript object with keys as arguments to our function, value will be return 
+
+// O(n) time and space complexity 
+const fib2 = (n, memo = {}) => {
+    // check to see if memo contains n value, if it does since it is already capture, we will return the value of the key or n
+    if (n in memo) return memo[n];
+    if(n <=2) return 1;
+    //store result of n inside our memo if not in memo
+    memo[n] =  fib2(n -1,memo) + fib2(n - 2, memo);
+    return memo[n]; 
+}
+
+console.log('Testing with memoization')
+console.log(fib2(7));
+console.log(fib2(30));
+console.log(fib2(46));
